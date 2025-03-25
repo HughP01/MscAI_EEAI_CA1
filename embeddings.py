941bd8f89ad2d3e8edac4f3ffe1cff3a9914b983
HughP01 <71641914+HughP01@users.noreply.github.com>
2025-03-25 09:56:46 +0000

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -8,7 +8,7 @@ np.random.seed(seed)
 
 def get_tfidf_embd(df:pd.DataFrame):
     from sklearn.feature_extraction.text import TfidfVectorizer
-    tfidfconverter = TfidfVectorizer(max_features=2000, min_df=4, max_df=0.90)
+    tfidfconverter = TfidfVectorizer(max_features=2000, min_df=1, max_df=0.7)
     data = df[Config.TICKET_SUMMARY] + ' ' + df[Config.INTERACTION_CONTENT]
     X = tfidfconverter.fit_transform(data).toarray()
     return X
